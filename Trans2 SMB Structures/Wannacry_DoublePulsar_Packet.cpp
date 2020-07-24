bf1cdf2925218a791f449798f8e293ec909ca028
bhassani <bhassani@users.noreply.github.com>
2020-07-24 13:15:07 -0500

    Update Wannacry_DoublePulsar_Packet.cpp

--- Trans2 SMB Structures/Wannacry_DoublePulsar_Packet.cpp
+++ Trans2 SMB Structures/Wannacry_DoublePulsar_Packet.cpp
@@ -28,24 +28,24 @@ typedef struct {
 
 //tried to add the types.  Should be accurate!
 typedef struct {
-  uint8_t	WordCount                 //15
-  uint16_t TotalParameterCount       //12
-  uint16_t TotalDataCount            //4096
-  uint16_t MaxParameterCount         //1
-  uint16_t MaxDataCount              //0
-  uint8_t MaxSetupCount             //0
-  uint8_t Reserved1                  //00
-  uint16_t Flags                     //0x0000
-  uint32_t Timeout                   //25 89 1a 00
-  uint16_t Reserved2                  //0000
-  uint16_t ParameterCount            //12
-  uint16_t ParameterOffset           //66
-  uint16_t DataCount                 //4096
-  uint16_t DataOffset                //78
-  uint8_t SetupCount                //1
-  uint8_t Reserved3                  //0
-  uint16_t Subcommand                //0x000e
-  uint16_t ByteCount                 //4109
+  uint8_t	WordCount;                 //15
+  uint16_t TotalParameterCount;       //12
+  uint16_t TotalDataCount;            //4096
+  uint16_t MaxParameterCount;         //1
+  uint16_t MaxDataCount;              //0
+  uint8_t MaxSetupCount ;            //0
+  uint8_t Reserved1;                  //00
+  uint16_t Flags;                     //0x0000
+  uint32_t Timeout;                   //25 89 1a 00
+  uint16_t Reserved2;                  //0000
+  uint16_t ParameterCount;            //12
+  uint16_t ParameterOffset;           //66
+  uint16_t DataCount;                 //4096
+  uint16_t DataOffset;                //78
+  uint8_t SetupCount;                //1
+  uint8_t Reserved3;                  //0
+  uint16_t Subcommand;                //0x000e
+  uint16_t ByteCount;                 //4109
   Padding                   //00
   Parameters[12]            //SESSION_SETUP Parameters
   Data[4096]                //SESSION_SETUP Data
