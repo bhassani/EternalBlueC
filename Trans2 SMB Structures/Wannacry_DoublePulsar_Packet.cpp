2f4225775f037d5af15bf11618e6586f2a78e910
bhassani <bhassani@users.noreply.github.com>
2020-07-24 13:13:43 -0500

    Update Wannacry_DoublePulsar_Packet.cpp

--- Trans2 SMB Structures/Wannacry_DoublePulsar_Packet.cpp
+++ Trans2 SMB Structures/Wannacry_DoublePulsar_Packet.cpp
@@ -28,24 +28,24 @@ typedef struct {
 
 //tried to add the types.  Should be accurate!
 typedef struct {
-  uint8	WordCount                 //15
-  uint16 TotalParameterCount       //12
-  uint16 TotalDataCount            //4096
-  uint16 MaxParameterCount         //1
-  uint16 MaxDataCount              //0
-  uint8 MaxSetupCount             //0
-  uint8 Reserved1                  //00
-  uint16 Flags                     //0x0000
-  uint32 Timeout                   //25 89 1a 00
-  uint16 Reserved2                  //0000
-  uint16 ParameterCount            //12
-  uint16 ParameterOffset           //66
-  uint16 DataCount                 //4096
-  uint16 DataOffset                //78
-  uint8 SetupCount                //1
-  uint8 Reserved3                  //0
-  uint16 Subcommand                //0x000e
-  uint16 ByteCount                 //4109
+  uint8_t	WordCount                 //15
+  uint16_t TotalParameterCount       //12
+  uint16_t TotalDataCount            //4096
+  uint16_t MaxParameterCount         //1
+  uint16_t MaxDataCount              //0
+  uint8_t MaxSetupCount             //0
+  uint8_t Reserved1                  //00
+  uint16_t Flags                     //0x0000
+  uint32_t Timeout                   //25 89 1a 00
+  uint16_t Reserved2                  //0000
+  uint16_t ParameterCount            //12
+  uint16_t ParameterOffset           //66
+  uint16_t DataCount                 //4096
+  uint16_t DataOffset                //78
+  uint8_t SetupCount                //1
+  uint8_t Reserved3                  //0
+  uint16_t Subcommand                //0x000e
+  uint16_t ByteCount                 //4109
   Padding                   //00
   Parameters[12]            //SESSION_SETUP Parameters
   Data[4096]                //SESSION_SETUP Data
