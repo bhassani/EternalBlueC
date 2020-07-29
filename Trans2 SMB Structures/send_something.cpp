f89d54e012bfb81f54dd2095a8f66ff48823fb98
bhassani <bhassani@users.noreply.github.com>
2020-07-28 23:21:16 -0500

    Update send_something.cpp

--- Trans2 SMB Structures/send_something.cpp
+++ Trans2 SMB Structures/send_something.cpp
@@ -1,3 +1,45 @@
+typedef struct _SMB_HEADER
+{
+	UCHAR    Protocol[4];
+	UCHAR    Command;
+	NTSTATUS Status;
+	UCHAR    Flags;
+	USHORT   Flags2;
+	USHORT   PIDHigh;
+	UCHAR    SecurityFeatures[8];
+	USHORT   Reserved;
+	USHORT   TID;
+	USHORT   PIDLow;
+	USHORT   UID;
+	USHORT   MID;
+} SMB_HEADER, * PSMB_HEADER;
+
+//
+// Header for reading oncoming requests
+// from client.
+//
+typedef struct _SMB_TRANS2_PARAM_HEADER
+{
+	UCHAR  WordCount;
+	struct Words
+	{
+		USHORT TotalParameterCount;
+		USHORT TotalDataCount;
+		USHORT Reserved1;
+		USHORT ParameterCount;
+		USHORT ParameterOffset;
+		USHORT ParameterDisplacement;
+		USHORT DataCount;
+		USHORT DataOffset;
+		USHORT DataDisplacement;
+		UCHAR  SetupCount;
+		UCHAR  Reserved2;
+		USHORT Setup[1];
+	};
+} SMB_TRANS2_HDR, * PSMB_TRANS2_HDR;
+
+//from somewhere else
+
 typedef struct {
 	uint16 SmbMessageType; //0x00
 	uint16 SmbMessageLength; 
