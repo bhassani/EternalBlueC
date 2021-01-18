7caac60b27c594e38c54657a71ed387f3711b709
bhassani <bhassani@users.noreply.github.com>
2021-01-18 16:47:25 -0600

    Update Test_DoublePulsar_Parameter_Loop.cpp

--- Test_DoublePulsar_Parameter_Loop.cpp
+++ Test_DoublePulsar_Parameter_Loop.cpp
@@ -16,15 +16,15 @@ void loop_through_buffer()
 	unsigned int Key = 0x58581162;
   
   //encrypted parameters here
-	unsigned int p_size = 0x507308 ^ Key;
-	unsigned int c_size = 4096 ^ Key;
-	unsigned int o_offset = 0 ^ Key;
+	unsigned int p_size = 0x507308 ^ Key; //total_payload_size here
+	unsigned int c_size = 4096 ^ Key; //chunk size
+	unsigned int o_offset = 0 ^ Key; //offset value, starts at 0
 
 //buffer to hold our parameters
 	char Parametersbuffer[12];
 
 	int i;
-  int ctx = 0; //counter
+  	int ctx = 0; //counter
 
 	unsigned int v19 = int(total_payload_size);
 	unsigned int v9 = v19 / 4096;
@@ -42,9 +42,9 @@ void loop_through_buffer()
 			{
 				break;
 			}
-			o_offset = ctx ^ Key; //update offset by ctx ( +4096 ) every loop
+			o_offset = ctx ^ Key; //update the offset ( +4096 ) every loop
       
-      //copy the new parameters
+      			//copy the new parameters
 			memcpy(Parametersbuffer, (char*)&p_size, 4);
 			memcpy(Parametersbuffer + 4, (char*)&c_size, 4);
 			memcpy(Parametersbuffer + 8, (char*)&o_offset, 4);
@@ -70,8 +70,12 @@ void loop_through_buffer()
 	{
 		printf("Last packet!  Bytes Left = %d\n", bytesLeft);
 
-		c_size = bytesLeft ^ Key; //update count size to the last bytes to upload
+					  //Normally the chunk size is 4096 but this is less
+					//since there is only less than 4096 bytes left
+		c_size = bytesLeft ^ Key; //update chunk size to the last bytes to upload
 		o_offset = ctx ^ Key; //update offset counter to the latest value before the break
+		
+		//copy the new parameters over
 		memcpy(Parametersbuffer, (char*)&p_size, 4);
 		memcpy(Parametersbuffer + 4, (char*)&c_size, 4);
 		memcpy(Parametersbuffer + 8, (char*)&o_offset, 4);
@@ -84,7 +88,7 @@ void loop_through_buffer()
 		}
 	}
   
-  //sample taken from the last byte and verifying that the value = correct expected value
+  //sample taken from the last packet and verifying that the value = correct expected value
   //this works :)
 	unsigned int lastByte = 0x5858126a ^ 0x58581162;
 	printf("\n Last bytes left:  0x%x\n", lastByte);
