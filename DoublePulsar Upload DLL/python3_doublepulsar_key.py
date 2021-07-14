4bd1fa2ba78b11189ee706aabf4aa8100564fb3b
bhassani <bhassani@users.noreply.github.com>
2021-07-14 13:36:47 -0500

    fix syntax issues & variable location

--- DoublePulsar Upload DLL/python3_doublepulsar_key.py
+++ DoublePulsar Upload DLL/python3_doublepulsar_key.py
@@ -5,14 +5,6 @@ import socket
 import struct
 import threading
 
-
-# Packets
-negotiate_protocol_request = binascii.unhexlify("00000085ff534d4272000000001853c00000000000000000000000000000fffe00004000006200025043204e4554574f524b2050524f4752414d20312e3000024c414e4d414e312e30000257696e646f777320666f7220576f726b67726f75707320332e316100024c4d312e325830303200024c414e4d414e322e3100024e54204c4d20302e313200")
-session_setup_request = binascii.unhexlify("00000088ff534d4273000000001807c00000000000000000000000000000fffe000040000dff00880004110a000000000000000100000000000000d40000004b000000000000570069006e0064006f007700730020003200300030003000200032003100390035000000570069006e0064006f007700730020003200300030003000200035002e0030000000")
-tree_connect_request = binascii.unhexlify("00000060ff534d4275000000001807c00000000000000000000000000000fffe0008400004ff006000080001003500005c005c003100390032002e003100360038002e003100370035002e003100320038005c00490050004300240000003f3f3f3f3f00")
-trans2_session_setup = binascii.unhexlify("0000004eff534d4232000000001807c00000000000000000000000000008fffe000841000f0c0000000100000000000000a6d9a40000000c00420000004e0001000e000d0000000000000000000000000000")
-
-
 def calculate_doublepulsar_xor_key(s):
     x = (2 * s ^ (((s & 0xff00 | (s << 16)) << 8) | (((s >> 16) | s & 0xff0000) >> 8)))
     x = x & 0xffffffff  # this line was added just to truncate to 32 bits
@@ -25,15 +17,19 @@ def calculate_doublepulsar_arch(s):
         return "x86 (32-bit)"
     else:
         return "x64 (64-bit)"
-      
-      
-      
-if __name___ == "__main__":
-    global negotiate_protocol_request, session_setup_request, tree_connect_request, trans2_session_setup, timeout, verbose
 
-    #sample IP
-    ip = "192.168.1.10"
-    
+
+if __name__ == "__main__":
+    # Packets
+    negotiate_protocol_request = binascii.unhexlify("00000085ff534d4272000000001853c00000000000000000000000000000fffe00004000006200025043204e4554574f524b2050524f4752414d20312e3000024c414e4d414e312e30000257696e646f777320666f7220576f726b67726f75707320332e316100024c4d312e325830303200024c414e4d414e322e3100024e54204c4d20302e313200")
+    session_setup_request = binascii.unhexlify("00000088ff534d4273000000001807c00000000000000000000000000000fffe000040000dff00880004110a000000000000000100000000000000d40000004b000000000000570069006e0064006f007700730020003200300030003000200032003100390035000000570069006e0064006f007700730020003200300030003000200035002e0030000000")
+    tree_connect_request = binascii.unhexlify("00000060ff534d4275000000001807c00000000000000000000000000000fffe0008400004ff006000080001003500005c005c003100390032002e003100360038002e003100370035002e003100320038005c00490050004300240000003f3f3f3f3f00")
+    trans2_session_setup = binascii.unhexlify("0000004eff534d4232000000001807c00000000000000000000000000008fffe000841000f0c0000000100000000000000a6d9a40000000c00420000004e0001000e000d0000000000000000000000000000")
+
+    timeout = 5.0
+    # sample IP
+    ip = "192.168.0.11"
+
     # Connect to socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(float(timeout) if timeout else None)
@@ -53,7 +49,7 @@ if __name___ == "__main__":
 
     # Extract user ID from session setup response
     user_id = session_setup_response[32:34]
-    print("User ID = %s" % struct.unpack("<H", user_id)[0]))
+    print("User ID = %s" % struct.unpack("<H", user_id)[0])
 
     # Replace user ID in tree connect request packet
     modified_tree_connect_request = list(tree_connect_request)
@@ -75,7 +71,7 @@ if __name___ == "__main__":
 
     # Extract tree ID from response
     tree_id = tree_connect_response[28:30]
-    print("Tree ID = %s" % struct.unpack("<H", tree_id)[0]))
+    print("Tree ID = %s" % struct.unpack("<H", tree_id)[0])
 
     # Replace tree ID and user ID in trans2 session setup packet
     modified_trans2_session_setup = list(trans2_session_setup)
@@ -103,7 +99,7 @@ if __name___ == "__main__":
     # Check for 0x51 response to indicate DOUBLEPULSAR infection
     if final_response[34] == "\x51":
         signature = final_response[18:26]
-        signature_long = struct.unpack('<Q', signature)[0]
-        key = calculate_doublepulsar_xor_key(signature_long)
-        arch = calculate_doublepulsar_arch(signature_long)
-        print("[+] [%s] DOUBLEPULSAR SMB IMPLANT DETECTED!!! Arch: %s, XOR Key: %s" % (ip, arch, hex(key)))
+    signature_long = struct.unpack('<Q', signature)[0]
+    key = calculate_doublepulsar_xor_key(signature_long)
+    arch = calculate_doublepulsar_arch(signature_long)
+    print("[+] [%s] DOUBLEPULSAR SMB IMPLANT DETECTED!!! Arch: %s, XOR Key: %s" % (ip, arch, hex(key)))
